febef9f4d465d9a463ecc591fc4ec3d213549830
Matt Rayner <matt@mattrayner.co.uk>
2014-08-14 23:28:57 +0100

    Updated API to only show the currently enabled puppies

--- app/controllers/api/v1/puppy_controller.rb
+++ app/controllers/api/v1/puppy_controller.rb
@@ -1,6 +1,6 @@
 class API::V1::PuppyController < ApplicationController
   def index
-    @puppies = Puppy.all
+    @puppies = Puppy.enabled
     respond_to do |format|
       format.json { render :json => @puppies, :callback => params['callback'], :only => [ :id, :orientation, :image ] }
     end
